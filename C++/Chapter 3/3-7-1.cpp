6ed6060c87f78c2df09060d0b31e3ced22872a4e
Racham Du <rachamdu@outlook.com>
2019-03-04 09:24:13 +0800

    Add files via upload

---
+++ C++/Chapter 3/3-7-1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int ftin(int);
+
+const int trans = 12;
+
+int main()
+{
+    int h = 0;
+    cout << "Enter your height: ______ in\b\b\b\b\b\b\b\b\b";
+    cin >> h;
+    cout << "Your height is ";
+    ftin(h);
+}
+
+int ftin(int n)
+{
+    int i = 0, j = 0;
+    i = n / trans;
+    j = n % trans;
+    cout << i << " ft " << j << " in." << endl;
+    return 0;
+}
