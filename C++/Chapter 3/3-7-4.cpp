f8468528112253c261ce4458034ebcd89149202b
Racham Du <rachamdu@outlook.com>
2019-03-05 19:53:02 +0800

    Add files via upload

---
+++ C++/Chapter 3/3-7-4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	long long sc = 0;
+	long long d = 0, h = 0, m = 0, s = 0;
+	cout << "Enter a number of seconds: ";
+	cin >> sc;
+	d = sc / 86400;
+	h = (sc % 86400) / 3600;
+	m = ((sc % 86400) % 3600) / 60;
+	s = (((sc % 86400) % 3600) % 60);
+	cout << sc << " seconds = " << d << " days, " << h << " hours, " << m << " minutes, " << s << " seconds." << endl;
+	return 0;
+}
