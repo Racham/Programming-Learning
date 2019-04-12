3aa9f56ae1d02c8fd4dc6948cc336e100f3c4e75
Racham Du <rachamdu@outlook.com>
2019-04-12 23:26:17 +0800

    Add files via upload

---
+++ C++/Chapter 4/4-13-7.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+struct Pisa {
+	string name;
+	double diameter;
+	double weight;
+};
+int main()
+{
+	Pisa nums;
+	cout << "Enter Pisa's name: ";
+	getline(cin, nums.name);
+	cout << "Enter Pisa's diameter: ";
+	cin >> nums.diameter;
+	cout << "Enter Pisa's weight: ";
+	cin >> nums.weight;
+	cout << "pisa's name: " << nums.name << endl;
+	cout << "pisa's weight: " << nums.weight << endl;
+	cout << "pisa's diameter: " << nums.diameter << endl;
+	return 0;
+}
