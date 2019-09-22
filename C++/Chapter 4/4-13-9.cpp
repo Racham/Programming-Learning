0d477688af82fbfda8c7b4d67de1cc65ba3ee08d
Racham Du <rachamdu@outlook.com>
2019-09-22 10:53:14 +0800

    Add files via upload

---
+++ C++/Chapter 4/4-13-9.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+struct CandyBar {
+	string brand;
+	double weight;
+	int calories;
+};
+int main()
+{
+	CandyBar *snack = new CandyBar [3];
+	snack[0] = {
+		"Big White Rabbit",
+		20.36,
+		500
+	};
+	snack[1] = {
+		"Coockie",
+		13.2,
+		98
+	};
+	snack[2] = {
+		"Flash Fly",
+		25.36,
+		430
+	};
+	for (int i = 0; i < 3; i++) {
+		cout << i+1 << endl;
+		cout << "snack's brand: " << snack[i].brand << endl;
+		cout << "snack's weight: " << snack[i].weight << endl;
+		cout << "snack's calories: " << snack[i].calories << endl;
+	}
+	delete [] snack;
+	return 0;
+}
